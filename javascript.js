33c87d45ddc5efc531a891a7f9db183ef0093fbd
Joe Giampaoli <giampaoli.joe@gmail.com>
2020-11-07 14:05:09 -0800

    api key

---
+++ javascript.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+// $("#submit").on("click", function(event) {
+
+//     event.preventDefault();
+
+//     var title = $("#").val();
+
+//     var limit = $("#").val();
+
+//     var startYear = $("#").val();
+
+//     var endYear = $("#").val();
+    
+//     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "&end_date=" + endYear + "&q=" + title + "&api-key=PUBT6xb5V1hiy7zwmHkiY7JlNTlGuRYk";
+    
+//     // date has to be formatted YYYYMMDD
+//     // api key PUBT6xb5V1hiy7zwmHkiY7JlNTlGuRYk
+    
+
+
+
+//     $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+
+//     var newArticle = $('<div>');
+//     newArticle.add;
+    
+//     var headline = $('<h5>');
+//     headline.text(respone.blah);
+    
+//     var thumbnail = $('<img>');
+
+//     })
+
+// }); 
